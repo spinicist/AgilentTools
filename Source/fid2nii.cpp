3d03c413b27c4738a93aa56c4dfb8145c60a884f
Tobias Wood <tobias@spinicist.org.uk>
2015-08-22 21:43:56 +0100

    Refactored so FFT and output code is reused.

--- Source/fid2nii.cpp
+++ Source/fid2nii.cpp
@@ -68,20 +68,15 @@ void fft_and_shift_Z(MultiArray<complex<float>, 3> &a) {
     }
 }
 
-void reconMGE(Agilent::FID &fid, const string &outpath, const bool kspace, const Nifti::DataType dtype);
-void reconMGE(Agilent::FID &fid, const string &outpath, const bool kspace, const Nifti::DataType dtype) {
+MultiArray<complex<float>, 4> reconMGE(Agilent::FID &fid);
+MultiArray<complex<float>, 4> reconMGE(Agilent::FID &fid) {
     int nx = fid.procpar().realValue("np") / 2;
     int ny = fid.procpar().realValue("nv");
     int nz = fid.procpar().realValue("nv2");
     int narray = fid.procpar().realValue("arraydim");
     int ne = fid.procpar().realValue("ne");
 
-    float lx = fid.procpar().realValue("lro") / nx;
-    float ly = fid.procpar().realValue("lpe") / ny;
-    float lz = fid.procpar().realValue("lpe2") / nz;
-
-    Nifti::Header outHdr(nx, ny, nz, narray * ne, lx, ly, lz, 1, dtype);
-    Nifti::File output(outHdr, outpath);
+    MultiArray<complex<float>, 4> vols({nx, ny, nz, narray*ne});
     int vol = 0;
     for (int a = 0; a < narray; a++) {
         cout << "Reading block " << a << endl;
@@ -89,37 +84,29 @@ void reconMGE(Agilent::FID &fid, const string &outpath, const bool kspace, const
         *block = fid.readBlock(a);
         int e_offset = 0;
         for (int e = 0; e < ne; e++) {
-            cout << "Processing echo " << e << endl;
-            MultiArray<complex<float>, 3> k({nx, ny, nz}, block, {1,ne*nx,ne*nx*ny}, e_offset);
+            cout << "Reading echo " << e << endl;
+            MultiArray<complex<float>, 3> this_vol({nx, ny, nz}, block, {1,ne*nx,ne*nx*ny}, e_offset);
+            MultiArray<complex<float>, 3> slice = vols.slice<3>({0,0,0,vol},{-1,-1,-1,0});
 
-            if (!kspace) {
-                fft_and_shift_X(k);
-                fft_and_shift_Y(k);
-                fft_and_shift_Z(k);
+            auto it1 = this_vol.begin();
+            auto it2 = slice.begin();
+            while (it1 != this_vol.end()) {
+                *it2++ = *it1++;
             }
-            cout << "Writing volume" << endl;
-            output.writeVolumes(k.begin(), k.end(), vol, 1);
             vol++;
             e_offset += nx;
         }
     }
-    output.close();
+    return vols;
 }
 
-void reconMP2RAGE(Agilent::FID &fid, const string &outpath, const bool kspace, const Nifti::DataType dtype);
-void reconMP2RAGE(Agilent::FID &fid, const string &outpath, const bool kspace, const Nifti::DataType dtype) {
+MultiArray<complex<float>, 4> reconMP2RAGE(Agilent::FID &fid);
+MultiArray<complex<float>, 4> reconMP2RAGE(Agilent::FID &fid) {
     int nx = fid.procpar().realValue("np") / 2;
     int ny = fid.procpar().realValue("nv");
     int nz = fid.procpar().realValue("nv2");
 
-    float lx = fid.procpar().realValue("lro") / nx;
-    float ly = fid.procpar().realValue("lpe") / ny;
-    float lz = fid.procpar().realValue("lpe2") / nz;
-
     ArrayXi pelist = fid.procpar().realValues("pelist").cast<int>();
-
-    Nifti::Header outHdr(nx, ny, nz, 2, lx, ly, lz, 1, dtype);
-    Nifti::File output(outHdr, outpath);
     MultiArray<complex<float>, 4> k({nx, ny, nz, 2});
 
     for (int z = 0; z < nz; z++) {
@@ -137,17 +124,7 @@ void reconMP2RAGE(Agilent::FID &fid, const string &outpath, const bool kspace, c
         }
     }
 
-    if (!kspace) {
-        for (int v = 0; v < 2; v++) {
-            MultiArray<complex<float>, 3> vol = k.slice<3>({0,0,0,v},{-1,-1,-1,0});
-            fft_and_shift_X(vol);
-            fft_and_shift_Y(vol);
-            fft_and_shift_Z(vol);
-        }
-    }
-    cout << "Writing volume" << endl;
-    output.writeVolumes(k.begin(), k.end(), 0, 2);
-    output.close();
+    return k;
 }
 
 int main(int argc, char **argv) {
@@ -202,14 +179,35 @@ int main(int argc, char **argv) {
         throw(runtime_error("apptype " + apptype + " not supported."));
     }
 
+    MultiArray<complex<float>, 4> vols;
     if (seqfil.substr(0, 5) == "mge3d") {
         cout << "MGE Recon" << endl;
-        reconMGE(fid, outPath, kspace, dtype);
+        vols = reconMGE(fid);
     } else if (seqfil.substr(0, 7) == "mp2rage") {
         cout << "mp2rage recon" << endl;
-        reconMP2RAGE(fid, outPath, kspace, dtype);
+        vols = reconMP2RAGE(fid);
     }
 
+    cout << "Dims " << vols.dims().transpose() << endl;
+    if (!kspace) {
+        for (int v = 0; v < vols.dims()[3]; v++) {
+            cout << "FFTing vol " << v << endl;
+            MultiArray<complex<float>, 3> vol = vols.slice<3>({0,0,0,v},{-1,-1,-1,0});
+            fft_and_shift_X(vol);
+            fft_and_shift_Y(vol);
+            fft_and_shift_Z(vol);
+        }
+    }
+    cout << "Writing volume" << endl;
+
+    float lx = fid.procpar().realValue("lro") / vols.dims()[0];
+    float ly = fid.procpar().realValue("lpe") / vols.dims()[1];
+    float lz = fid.procpar().realValue("lpe2") / vols.dims()[2];
+    ArrayXf voxdims(4); voxdims << lx, ly, lz, 1;
+    Nifti::Header outHdr(vols.dims(), voxdims, dtype);
+    Nifti::File output(outHdr, outPath);
+    output.writeVolumes(vols.begin(), vols.end(), 0, vols.dims()[3]);
+    output.close();
     return 0;
 }
 
